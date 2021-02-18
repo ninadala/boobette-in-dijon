958b81fbdfaf0edd07680843d1a97d32b956171c
ninadala <nina.sellal@hotmail.fr>
2021-02-18 14:37:05 +0100

    index.php without .css and .jpeg

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
 <?php
 
-require_once('style.css');
-
-require_once('html/index.html');
-require_once('html/asso.html');
-require_once('html/dates.html');
-require_once('html/donations.html');
-require_once('html/partnerships.html');
-
-require_once('pictures/boobs.JPG');
-require_once('pictures/elle-army.jpeg');
-require_once('pictures/ellen.jpeg');
-require_once('pictures/gala.jpg');
-require_once('pictures/leaflet-ellen.png');
-require_once('pictures/ppsc.jpeg');
-require_once('pictures/renarde-bouclee.jpg');
-require_once('pictures/stripes.jpeg');
-require_once('pictures/team-boobette.jpg');
-require_once('pictures/tmg21.jpeg');
-require_once('pictures/wootton-yarnbombing.jpeg');
-require_once('pictures/yarnbombing.jpeg');
+require_once('index.html');
+require_once('asso.html');
+require_once('dates.html');
+require_once('donations.html');
+require_once('partnerships.html');
